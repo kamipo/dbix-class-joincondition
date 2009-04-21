08f00021b8897f9786f99ceced295f2572407a0c
kamipo <kamipo@gmail.com>
2009-04-21 21:16:02 +0900

    first commit

--- lib/DBIx/Class/JoinCondition.pm
+++ lib/DBIx/Class/JoinCondition.pm
@@ -4,6 +4,36 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+{
+    use DBIx::Class::ResultSet ();
+
+    my $orig = \&DBIx::Class::ResultSet::_resolved_attrs;
+
+    my $method = sub {
+        my $self = shift;
+        my $attrs = $orig->($self);
+
+        my $on = $attrs->{on} or return $attrs;
+        my $from = $attrs->{from};
+
+        for (my $i = 1; $i < @$from; $i++) {
+            my $join = $from->[$i];
+            my @keys = sort keys %{$join->[0]};
+            my $key  = pop(@keys);
+            if (defined $on->{$key} and ref $on->{$key} eq 'HASH') {
+                my $cond = $on->{$key};
+                $join->[1] = {%{$join->[1]}, %$cond};
+            }
+        }
+
+        return $self->{_attrs} = $attrs;
+    };
+
+    no strict 'refs';
+    no warnings 'redefine';
+    *{'DBIx::Class::ResultSet::_resolved_attrs'} = $method;
+}
+
 1;
 __END__
 
